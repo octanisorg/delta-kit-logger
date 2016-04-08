5841cc29ba4785b14819597721946afde3ccd777
Sam Sulaimanov <sam@bitmorse.com>
2016-04-08 16:47:07 -0700

    Update axon.js

--- axon.js
+++ axon.js
@@ -4,4 +4,15 @@
 	App.load('home');
 
 
+	setInterval(function(){
+	
+		$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/BLE", function( data ) {
+			
+			$("#sensordata").html(data);
+			console.log(data)
+			
+		});
+		
+		
+	}, 1000);
 })();
